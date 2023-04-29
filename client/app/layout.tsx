d816f110a68c18d7bbfc22a32bd077380d18dc5c
jiin9999 <cji3604@gmail.com>
2023-04-30 00:52:05 +0900

    Feat: "PWA 구현"

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -4,7 +4,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="ko">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/splashscreens.png"
+        />
+        <meta name="msapplication-TileColor" content="#D4E8D8" />
+        <meta name="theme-color" content="#D4E8D8" />
+      </head>
       <body>{children}</body>
     </html>
   );
--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
   experimental: {
     appDir: true,
   },
-};
-
-module.exports = nextConfig;
+});
