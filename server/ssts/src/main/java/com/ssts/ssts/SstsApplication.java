ded63ff4f28b2a8ef458f9c19c59b2a00678146b
Gomuang <97213554+NewfileDOTpy@users.noreply.github.com>
2023-05-04 13:28:10 +0900

    feat : 이니셜 코드 추가
    이니셜 코드 추가

---
+++ server/ssts/src/main/java/com/ssts/ssts/SstsApplication.java
@@ -0,0 +1,13 @@
+package com.ssts.ssts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SstsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SstsApplication.class, args);
+	}
+
+}
---
+++ server/ssts/src/test/java/com/ssts/ssts/SstsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssts.ssts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SstsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
