41f313a5f8df17fc117c81b08e5f1017acb40d01
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-05-09 01:50:21 +0900

    Feat: Vote 컴포넌트로 분리

---
+++ client/app/detail/Vote.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import styled from "styled-components";
+import { StyledCard } from "./Card";
+import { FaRegThumbsUp, FaRegThumbsDown } from "react-icons/fa";
+
+export const Vote = () => {
+  return (
+    <StyledVote>
+      <h3 className="sub-title">그만 써도 될까요?</h3>
+      <StyledCard className="vote-box">
+        <div className="vote">
+          <div className="text">네!</div>
+          <FaRegThumbsUp className="icon" />
+        </div>
+        <div className="vote">
+          <FaRegThumbsDown className="icon" />
+          <div className="text">안돼요</div>
+        </div>
+      </StyledCard>
+    </StyledVote>
+  );
+};
+
+const StyledVote = styled.div`
+  .sub-title {
+    margin: 0 24px;
+  }
+  .vote-box {
+    display: flex;
+    flex-direction: row;
+    .vote {
+      cursor: pointer;
+      display: flex;
+      flex-direction: row;
+      margin: 0 12px;
+      .text {
+        margin: 0 8px;
+      }
+      .icon {
+        border: 50%;
+        /* padding: 12px; */
+        /* border: solid; */
+      }
+    }
+  }
+`;
--- client/app/detail/page.tsx
+++ client/app/detail/page.tsx
@@ -2,55 +2,20 @@
 
 import styled from "styled-components";
 import { Header } from "./Header";
-import { StyledCard } from "./Card";
 import { Slider } from "./Slider";
-import { FaRegThumbsUp, FaRegThumbsDown } from "react-icons/fa";
+import { Vote } from "./Vote";
+import { StyledCard } from "./Card";
 
 export default function Detail() {
   return (
-    <StyledDetail>
+    <>
       <Header />
       <Slider />
-      <h3 className="sub-title">그만 써도 될까요?</h3>
-      <StyledCard className="vote-box">
-        <div className="vote">
-          <div className="text">네!</div>
-          <FaRegThumbsUp className="icon" />
-        </div>
-        <div className="vote">
-          <FaRegThumbsDown className="icon" />
-          <div className="text">안돼요</div>
-        </div>
-      </StyledCard>
-
+      <Vote />
       <h3 className="sub-title">댓글 달기</h3>
       <StyledCard className="comment">
         <h3 className="sub-title">댓글 달기</h3>
       </StyledCard>
-    </StyledDetail>
+    </>
   );
 }
-
-const StyledDetail = styled.div`
-  .sub-title {
-    margin: 0 24px;
-  }
-  .vote-box {
-    display: flex;
-    flex-direction: row;
-    .vote {
-      cursor: pointer;
-      display: flex;
-      flex-direction: row;
-      margin: 0 12px;
-      .text {
-        margin: 0 8px;
-      }
-      .icon {
-        border: 50%;
-        /* padding: 12px; */
-        /* border: solid; */
-      }
-    }
-  }
-`;
