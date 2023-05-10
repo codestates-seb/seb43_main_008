dc01d3efd51db1e01f7cca5afe951662ac7a35a4
Gomuang <97213554+NewfileDOTpy@users.noreply.github.com>
2023-05-10 18:18:09 +0900

    feat : Series paging 기능 추가

--- server/ssts/src/main/java/com/ssts/ssts/domain/series/repository/SeriesRepository.java
+++ server/ssts/src/main/java/com/ssts/ssts/domain/series/repository/SeriesRepository.java
@@ -1,6 +1,6 @@
 package com.ssts.ssts.domain.series.repository;
 
-import com.ssts.ssts.domain.daylog.entity.Daylog;
+
 import com.ssts.ssts.domain.series.entity.Series;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Repository
 public interface SeriesRepository extends JpaRepository<Series, Long> {
-    Page<Daylog> findAllDaylogsById(Long seriesId, Pageable pageable);
+    Page<Series> findByMember_id(Long memberId, Pageable pageable);
 
     /* 멤버 서비스에서 멤버 아이디에 해당하는 시리즈 목록 출력 목적 */
     List<Series> findAllByMemberId(long memberId);
