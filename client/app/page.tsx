f5b035e6700f0cf65d16cb2d3dce073c0c28f1c0
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-05-14 02:25:48 +0900

    Feat: 메인페이지를 홈 라우터로 변경

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <div>MainPage Tests</div>;
+import { Filter } from "./main/Filter"
+import { Header } from "./main/Header";
+import { Lists } from "./main/Lists"
+import styles from "./main/page.module.css";
+import { Scroll } from "./main/Scroll"
+
+
+export default function Main() {
+  return (
+    <main className={`${styles.container}`} >
+      <Header />
+      <Filter />
+      <Lists />
+      <Scroll />
+    </main>
+  );
 }
