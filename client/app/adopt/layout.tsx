e7abadd5035393e15b301aa4ccd12e9be0b3eb6c
jungsoonhyun <bonoyah4@gmail.com>
2023-05-17 15:51:00 +0900

    Feat: 플라스틱 이름 정하는 페이지 레이아웃 정리

---
+++ client/app/adopt/layout.tsx
@@ -0,0 +1,21 @@
+"use client";
+import Header from "../Header";
+import Navbar from "../Navbar";
+
+export default function SeriesLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header
+        backButton={true}
+        textContent={"플라스틱 육아 시작하기"}
+        secretButton={false}
+      />
+      {children}
+      <Navbar />
+    </>
+  );
+}
