4f02b7d8c3b89391b7b0d58d216240b09c2fb709
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-05-21 16:10:57 +0900

    Feat: 댓글 서버 연결 중간 저장

--- client/app/my-page/EmptyFeed.tsx
+++ client/app/my-page/EmptyFeed.tsx
@@ -1,7 +1,7 @@
 "use client";
 import styled from "styled-components";
 
-export const EmptyFeed = (content: string) => {
+export const EmptyFeed = () => {
   return (
     <StyledEmptyFeed>
       <div> 작성된 글이 없습니다. </div>
