0ffce9dd33e460ca95a11e54b1145d445b598b44
mewluee <yunide073@gmail.com>
2023-05-21 14:19:36 +0900

    test : test를 위해 리다이렉트 주소 수정

--- server/ssts/src/main/java/com/ssts/ssts/global/auth/config/WebConfig.java
+++ server/ssts/src/main/java/com/ssts/ssts/global/auth/config/WebConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*") // 허용할 오리진(도메인) 설정
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드 설정
                 .exposedHeaders("Authorization") // 노출할 응답 헤더 설정
+                .exposedHeaders("Refresh") // 노출할 응답 헤더 설정
                 .allowCredentials(true) // 인증 정보 허용 여부 설정
                 .maxAge(3600); // preflight 요청 결과 캐싱 시간 설정
     }
