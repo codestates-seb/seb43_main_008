b2d76e63c528e11d97c1a9fab968f72d3cb7e55b
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-05-23 02:09:44 +0900

    Feat: 팔로우페이지 언팔로우&팔로우 이벤트 추가, 마이페이지 연결 추가

--- client/app/follow/Following.tsx
+++ client/app/follow/Following.tsx
@@ -1,7 +1,10 @@
 "use client";
-
 import Image from 'next/image';
+import { useRouter } from "next/navigation";
+import { useState } from 'react';
 import styled from "styled-components";
+
+import { DeleteUnFollowing, PostFollowing } from "../api/followApi"
 
 interface FollowProps {
   image: string;
@@ -9,27 +12,56 @@ interface FollowProps {
   introduce: string;
 }
 export const Follow: React.FC<FollowProps> = ({ image, nickName, introduce }) => {
+  const [isfollowing, setIsfollowing] = useState<boolean>(true);
+
+  const router = useRouter();
+
+  const moveToMypageHandler = (nickName: string) => {
+    router.push(`my-page/${nickName}`)
+    sessionStorage.setItem("header", nickName);
+  }
+
   return (
     <StyledFollow>
       <div className='profile-box'>
-        <Image
-          className='image'
-          src={image}
-          width={40}
-          height={40}
-          alt="프로필 사진"
-        />
-        <div className='info-box'>
-          <div className='nick-name'>{nickName}</div>
-          <div className='user-info'>{introduce}</div>
+        <div className='user-box' onClick={() => moveToMypageHandler(nickName)}>
+          <Image
+            className='image'
+            src={image}
+            width={40}
+            height={40}
+            alt="프로필 사진"
+          />
+          <div className='info-box'>
+            <div className='nick-name'>{nickName}</div>
+            <div className='user-info'>{introduce}</div>
+          </div>
         </div>
-        <Image
-          className='icon'
-          src="/icons/HeartFill.svg"
-          width={26}
-          height={26}
-          alt="팔로우 아이콘"
-        />
+
+        {isfollowing
+          ? <Image
+            className='icon'
+            src="/icons/HeartFill.svg"
+            width={26}
+            height={26}
+            alt="팔로우 아이콘"
+            onClick={() => {
+              DeleteUnFollowing(nickName)
+              setIsfollowing(false)
+            }}
+          />
+          : <Image
+            className='icon'
+            src="/icons/Heart.svg"
+            width={26}
+            height={26}
+            alt="팔로우 아이콘"
+            onClick={() => {
+              PostFollowing(nickName)
+              setIsfollowing(true)
+            }}
+          />}
+
       </div>
     </StyledFollow>
   )
@@ -42,14 +74,17 @@ const StyledFollow = styled.div`
   flex-direction: column;
   justify-content: center;
   width: 100vw;
-
   .profile-box{
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: flex-start;
   }
-
+  .user-box {
+    cursor: pointer;
+    display: flex;
+    flex-direction: row;
+  }
   .info-box{
     flex-grow: 8;
     display: flex;
@@ -62,9 +97,11 @@ const StyledFollow = styled.div`
     color: #757575;
     }
   }
-
   .image {
     border-radius: 50%;
     margin-right: 18px;
+  }
+  .icon{
+    cursor: pointer;
   }
 `
