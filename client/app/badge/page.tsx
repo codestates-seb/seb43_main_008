507d081190648c5b2a15991e3b0a44c108974855
jungsoonhyun <120349583+jungsoonhyun@users.noreply.github.com>
2023-05-24 03:44:17 +0900

    Feat: 뱃지 수정중

--- client/app/badge/page.tsx
+++ client/app/badge/page.tsx
@@ -1,28 +1,93 @@
 "use client";
 
+// import styled from "styled-components";
+
+// import Badge from "./Badge";
+// // import styles from "./page.module.css";
+
+// export default function page() {
+//   return (
+//     <>
+//       <MainSeriesContainer>
+//         <SeriesWrapper>
+//           <Badge />
+//         </SeriesWrapper>
+//       </MainSeriesContainer>
+//     </>
+//   );
+// }
+// // 페이지 기본 레이아웃
+// const MainSeriesContainer = styled.div`
+//   box-sizing: border-box;
+//   padding: 1px 24px;
+//   max-width: 1024px;
+//   margin: 0 auto;
+//   background-color: #ffff;
+//   min-height: 550px;
+//   min-width: 320px;
+//   @media screen and (max-width: 768px) {
+//   }
+// `;
+
+// 콘텐츠 박스 레이아웃
+// const SeriesWrapper = styled.div`
+//   margin: 0 1px;
+//   padding: 2px;
+// `;
+
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
+import axiosInstance from "../axiosInstance";
 import Badge from "./Badge";
-// import styles from "./page.module.css";
 
-export default function page() {
+export interface BadgeType {
+  badgeId: number;
+  isAcquired: boolean;
+  img: string;
+}
+
+// interface BadgeResponse {
+//   message: string;
+//   code: number;
+//   data: BadgeType[];
+// }
+
+export default function Page() {
+  const [badgeList, setBadgeList] = useState([]);
+
+  useEffect(() => {
+    const fetchBadgeList = async () => {
+      try {
+        const response = await axiosInstance.get(`/members/badge`);
+        console.log(response.data.data);
+        setBadgeList(response.data.data);
+      } catch (error) {
+        console.error("Error fetching badge list:", error);
+      }
+    };
+
+    fetchBadgeList();
+  }, []);
+
   return (
     <>
       <MainSeriesContainer>
         <SeriesWrapper>
-          <Badge />
+          <Badge badgeList={badgeList} />
         </SeriesWrapper>
       </MainSeriesContainer>
     </>
   );
 }
+
 // 페이지 기본 레이아웃
 const MainSeriesContainer = styled.div`
   box-sizing: border-box;
   padding: 1px 24px;
   max-width: 1024px;
   margin: 0 auto;
-  background-color: #fafafa;
+  background-color: #ffff;
   min-height: 550px;
   min-width: 320px;
   @media screen and (max-width: 768px) {
