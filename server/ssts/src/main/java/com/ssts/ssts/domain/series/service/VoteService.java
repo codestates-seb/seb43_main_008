81228f1f4f5baf4603cb0080f2d5e635e760f8fa
digital-hamster <waterlove1439@naver.com>
2023-05-24 10:13:14 +0900

    fix: getFeedSeries{nickName} 추가 및 수정

--- server/ssts/src/main/java/com/ssts/ssts/domain/series/service/VoteService.java
+++ server/ssts/src/main/java/com/ssts/ssts/domain/series/service/VoteService.java
@@ -71,9 +71,9 @@ public class VoteService {
         targetSeries.setVoteCreatedAt(LocalDateTime.now());
         //투표 마감기간 (2일) 할당
         //targetSeries.setVoteEndAt(targetSeries.getVoteCreatedAt().plusDays(2));
-        targetSeries.setVoteEndAt(targetSeries.getVoteCreatedAt().plusSeconds(40));
+        //targetSeries.setVoteEndAt(targetSeries.getVoteCreatedAt().plusSeconds(40));
         //targetSeries.setVoteEndAt(targetSeries.getVoteCreatedAt().plusHours(12));
-        //targetSeries.setVoteEndAt(targetSeries.getVoteCreatedAt().plusMinutes(5));
+        targetSeries.setVoteEndAt(targetSeries.getVoteCreatedAt().plusMinutes(5));
         //ㄴ> 테스트 마감기간: 15초
 
         //재투표시에 memberVote 초기화 (중복 제거)
