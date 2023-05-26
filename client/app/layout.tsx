6ddff286cd789def3010963631947dc6757370a0
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-05-26 23:13:18 +0900

    Feat: 검색 엔진 메타 태그 설정

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -25,12 +25,15 @@ export default async function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1, user-scalable=no"
         />
-
+        {/* 타이틀 설정 */}
+        <meta http-equiv="Title" content="쓰쓰또쓰" />
         {/* 오픈그래프 설정 */}
         <meta property="og:title" content="쓰쓰또쓰" />
-        <meta property="og:description" content="쓰고 쓰고 또 쓰자 &lt;br/&gt; 플라스틱 재사용 과정을 공유하는 서비스입니다." />
-        <meta property="og:image" content="../public/icons/icon-128x128.png" />
-
+        <meta property="og:description" content="쓰고 쓰고 또 쓰자 &lt;br/&gt; 플라스틱 재사용 과정을 공유하는 커뮤니티 서비스입니다." />
+        <meta property="og:image" content="/Users/areumoh/Documents/projects/seb43_main_008_2/seb43_main_008/client/public/icons/icon-128x128.png" />
+        {/* 검색 엔진 설정 */}
+        <meta name="Keywords" content="쓰쓰또쓰" />
+        <meta name="Description" content="쓰고 쓰고 또 쓰자. 플라스틱 재사용 과정을 공유하는 커뮤니티 서비스입니다." />
       </head>
       <body>
         <StyledComponentsRegistry>
