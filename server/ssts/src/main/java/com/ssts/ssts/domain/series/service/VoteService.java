156dfbd090f8b1552625dfdd10972c1d966492bd
digital-hamster <waterlove1439@naver.com>
2023-05-30 21:37:28 +0900

    fix: feed 상태값 문제 수정 - 투표 강제종료 뒤 시리즈들을 조회시 상태값 변경 문제 해결

--- server/ssts/src/main/java/com/ssts/ssts/domain/series/service/VoteService.java
+++ server/ssts/src/main/java/com/ssts/ssts/domain/series/service/VoteService.java
@@ -41,7 +41,7 @@ public class VoteService {
 
         if(memberId != targetSeries.getMember().getId()){ throw new BusinessLogicException(ExceptionCode.NOT_SERISE_WRITER); }
 
-
+        //데이로그가 0개면 예외 발생
         if(targetSeries.getDaylogCount()==0){throw new BusinessLogicException(ExceptionCode.VOTE_NOT_CREATE_DAYLOG);}
 
         //(voteCount>2) 더이상 투표를 개설할 수 없습니다
@@ -198,6 +198,11 @@ public class VoteService {
             //투표 종료에 대한 권한이 없습니다
             throw new BusinessLogicException(ExceptionCode.NOT_HAVE_VOTE_AUTHORITY);
         }
+
+        //강제 투표 종료 중복 방지 => 프론트가 만들어달라 할 때 해당 로직을 풉니다
+//        if(targetSeries.getVoteStatus()== Series.VoteStatus.SERIES_QUIT){
+//            throw new BusinessLogicException(ExceptionCode.THIS_VOTE_RESULT_IS_TRUE);
+//        }
 
 
         //[리팩토링 진입점: voteCount는 2부터 걸러지기 때문에, 1밖에 안들어옴. 조건에 1이 달릴 이유가 있나?]
