acf2cb1e6f1ebe7a1127ee93bc179f5a9b28205a
digital-hamster <waterlove1439@naver.com>
2023-05-30 20:15:15 +0900

    fix: vote Exception 추가2 - 데이로그가 0개일 때, 투표 개설 불가 예외 추가

--- server/ssts/src/main/java/com/ssts/ssts/global/exception/ExceptionCode.java
+++ server/ssts/src/main/java/com/ssts/ssts/global/exception/ExceptionCode.java
@@ -50,6 +50,7 @@ public enum ExceptionCode {
     NOT_SERISE_WRITER(RtnHttpStatus.Validation,1012, "시리즈를 작성한 본인이 아니라서 투표를 개설할 수 없어요."),
     VOTE_RESULT_NOT_UPDATE(RtnHttpStatus.Validation,1014,"최초투표의 결과가 조회되지 않았어요."),
     VOTE_ALREADY_FINISH(RtnHttpStatus.Validation,1015,"이미 투표가 종료되어 더 이상의 투표를 할 수 없어요."),
+    VOTE_NOT_CREATE_DAYLOG(RtnHttpStatus.Validation,1016,"투표를 개설하기 전, 데이로그를 작성해 주세요."),
 
     //북마크 에러
     BOOKMARK_IS_DUPLICATION(RtnHttpStatus.Validation,1009, "이미 북마크한 시리즈예요."),
