9aa144f2f18740f780aa8918d43405a60086957b
Gomuang <97213554+NewfileDOTpy@users.noreply.github.com>
2023-05-30 20:34:46 +0900

    debug : Daylog 오류 수정

--- server/ssts/src/main/java/com/ssts/ssts/domain/series/constant/SeriesConstants.java
+++ server/ssts/src/main/java/com/ssts/ssts/domain/series/constant/SeriesConstants.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SeriesConstants {
 
-    FILE_DiRECTORY("series/series-image.png"),
+    FILE_DiRECTORY("series/series-image2.png"),
 
     BUCKET_NAME("ssts-img");
 
